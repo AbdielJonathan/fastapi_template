e6540d459bc2cd488d06621d2ec76b25fc315355
AbdielJonathan <abdiel@buholegal.com>
2026-07-04 02:41:11 -0600

    comentamos el sexo

--- app/models/user.py
+++ app/models/user.py
@@ -20,7 +20,7 @@ class User(Base):
     email: Mapped[str] = mapped_column(
         String(255), unique=True, index=True, nullable=False
     )
-    sexo: Mapped[str] = mapped_column(CHAR(1), nullable=False)
+    # sexo: Mapped[str] = mapped_column(CHAR(1), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, server_default=func.now(), nullable=False
